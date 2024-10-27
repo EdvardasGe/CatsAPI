ebd352a0bb542eda498253d247689cb86cc1639a
EdvardasGe <edvardasgervaitis@gmail.com>
2024-10-28 00:51:24 +0200

    Fixed integration tests with postgres using testcontainers, can also adjust to work with H2 profile

--- src/test/groovy/com/edvardas/CatsAPI/controller/CatControllerSpec.groovy
+++ src/test/groovy/com/edvardas/CatsAPI/controller/CatControllerSpec.groovy
@@ -7,7 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
 import org.springframework.test.web.servlet.MockMvc
+import org.testcontainers.containers.PostgreSQLContainer
 import spock.lang.Specification
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
@@ -15,8 +19,25 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf
 
 @SpringBootTest
+@ActiveProfiles("prod")
 @AutoConfigureMockMvc
 class CatControllerSpec extends Specification {
+
+    static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:12")
+            .withDatabaseName("catsdb")
+            .withUsername("admin")
+            .withPassword("admin")
+
+    static {
+        postgresContainer.start()
+    }
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl)
+        registry.add("spring.datasource.username", postgresContainer::getUsername)
+        registry.add("spring.datasource.password", postgresContainer::getPassword)
+    }
 
     @Autowired
     MockMvc mockMvc
