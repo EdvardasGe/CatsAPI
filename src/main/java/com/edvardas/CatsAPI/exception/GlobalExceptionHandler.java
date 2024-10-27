4a41e9e82860e304d338a0ac8738e7b8bece2ec3
EdvardasGe <edvardasgervaitis@gmail.com>
2024-10-27 21:53:06 +0200

    Add working swagger UI, small unused clear on exception handler

--- src/main/java/com/edvardas/CatsAPI/exception/GlobalExceptionHandler.java
+++ src/main/java/com/edvardas/CatsAPI/exception/GlobalExceptionHandler.java
@@ -2,12 +2,8 @@ package com.edvardas.CatsAPI.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
