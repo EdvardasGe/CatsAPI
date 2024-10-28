78ca6fb507fab5167d440cb95e4f950d8b54606c
EdvardasGe <edvardasgervaitis@gmail.com>
2024-10-28 02:24:27 +0200

    remove unused import

--- src/test/groovy/com/edvardas/CatsAPI/constants/TestConstants.java
+++ src/test/groovy/com/edvardas/CatsAPI/constants/TestConstants.java
@@ -1,7 +1,5 @@
 package com.edvardas.CatsAPI.constants;
 
-import com.edvardas.CatsAPI.model.Cat;
-
 public class TestConstants {
     public static final String USERNAME = "user";
     public static final String PASSWORD = "password";
